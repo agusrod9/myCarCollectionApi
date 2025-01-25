7b824c8c2e5f9602c3f0c53feacfe5a65b625fec
Agustin <agusrod9@gmail.com>
2025-01-24 21:34:16 -0300

    Nuevas escalas disponibles

--- src/dao/models/cars.model.js
+++ src/dao/models/cars.model.js
@@ -13,7 +13,7 @@ const schema = new mongoose.Schema({
     manufacturer : {type : String, default : null},
     notes : {type : String, default : null},
     opened : {type: String, enum: ['opened', 'sealed'], default: null},
-    scale : {type: String, enum:[ '1/5','1/8','1/10', '1/12', '1/18', '1/24', '1/32', '1/36', '1/43', '1/64', '1/72', '1/87', '1/144', '1/160' ], required: true},
+    scale : {type: String, enum:['1/4', '1/5', '1/6', '1/8', '1/10', '1/12', '1/18', '1/24', '1/32', '1/36', '1/43', '1/48', '1/50', '1/55', '1/60', '1/64', '1/70', '1/72', '1/76', '1/87', '1/100', '1/120', '1/148', '1/160', '1/200'], required: true},
     series : {type : String, default : null },
     series_num : {type : String, default : null },
     userId : {type: mongoose.Schema.Types.ObjectId, ref: 'users', required: true},
