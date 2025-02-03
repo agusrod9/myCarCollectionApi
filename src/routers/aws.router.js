d96f9fb0c25d8126930c33605b1d471edf785687
Agustin <agusrod9@gmail.com>
2025-02-03 07:56:20 -0300

    Agrego endpoint de AWS para borrar imágenes de los autos por ID

--- src/routers/aws.router.js
+++ src/routers/aws.router.js
@@ -1,7 +1,7 @@
 import { Router } from "express";
 import sharp from "sharp";
 import multer from "multer";
-import { S3Client, PutObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3";
+import { S3Client, PutObjectCommand, GetObjectCommand, DeleteObjectCommand } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import crypto from 'crypto';
 
@@ -42,11 +42,25 @@ router.post('/', upload.single('image'), async(req,res)=>{
     }
     const getCommand = new GetObjectCommand(getParams);
     let url = await getSignedUrl(s3, getCommand, { expiresIn: 3600 });
-    url = url.split('?')[0]
+    url = url.split('?')[0];
 
 
-    return res.status(200).json({url})
+    return res.status(200).json({url});
     
+});
+
+router.delete('/', async(req,res)=>{
+    const {id} = req.query; // Example 4a546a8bc374ffc7c5b170fce9ab538d from image url
+
+    const deleteParams = {
+        Bucket : bucketName,
+        Key : id
+    }
+
+    const deleteCommand = new DeleteObjectCommand(deleteParams);
+    await s3.send(deleteCommand)
+    return res.status(200).json({error: null, data : id});
+
 });
 
 export default router
