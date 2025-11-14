68650c1062d76bb6a56ab862a93120516e85423a
Agustin <agusrod9@gmail.com>
2025-11-14 18:17:27 -0300

    CARSMODEL - redefined price type for a complex object with currency and amount

--- src/dao/models/cars.model.js
+++ src/dao/models/cars.model.js
@@ -1,4 +1,4 @@
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
 
 mongoose.pluralize(null);
 
@@ -12,7 +12,24 @@ const schema = new mongoose.Schema({
     img_urls : [{type : String, default:[]}],
     manufacturer : {type : String, default : null},
     notes : {type : String, default : null},
-    price : {type: Number, default: 0},
+    price : {
+        type: new mongoose.Schema({
+            currency : {
+                type: mongoose.Schema.Types.ObjectId,
+                ref: 'currencies',
+                required: function(){
+                    return this.price != null;
+                }
+            },
+            amount : {
+                type: Number,
+                required: function(){
+                    return this.price != null;
+                }
+            }
+        }, {_id: false}),
+        default:null   
+    },
     opened : {type: String, enum: ['opened', 'sealed'], default: null},
     scale : {type: String, enum:['1/4', '1/5', '1/6', '1/8', '1/10', '1/12', '1/18', '1/24', '1/32', '1/36', '1/43', '1/48', '1/50', '1/55', '1/60', '1/61', '1/64', '1/70', '1/72', '1/76', '1/87', '1/100', '1/120', '1/148', '1/160', '1/200'], required: true},
     series : {type : String, default : null },
