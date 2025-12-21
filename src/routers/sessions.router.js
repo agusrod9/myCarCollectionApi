4f837e3f8fb26470c2260af3e1be37abda6ae76b
Agustin <agusrod9@gmail.com>
2025-12-20 22:30:22 -0300

    Add more stats to users model

--- src/routers/sessions.router.js
+++ src/routers/sessions.router.js
@@ -110,7 +110,8 @@ async function onlineUserData(req, res, next){
             nickName : user.nickName,
             profilePicture : user.profilePicture,
             role : user.role,
-            socialLinks : user.socialLinks
+            socialLinks : user.socialLinks,
+            stats: user.stats
         }
         return res.status(200).json({error : null, data : safeUser})
     } catch (error) {
